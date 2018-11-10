a18e1b1de90bd893807b26f6f809c2397af74321
romanb123 <romanb123@users.noreply.github.com>
2018-11-10 17:19:34 +0200

    add jsprj

---
+++ jsp.js
@@ -0,0 +1,33 @@
+
+var show=document.getElementById('showcase');
+var butred=document.getElementById('red');
+var butgreen=document.getElementById('green');
+var butblue=document.getElementById('blue');
+var butyellow=document.getElementById('yellow');
+
+butred.onclick=function(){
+    show.setAttribute('class','screenRed');
+};
+butgreen.onclick=function(){
+    show.setAttribute('class','screenGreen');
+}
+butblue.onclick=function(){
+    show.setAttribute('class','screenBlue');
+}
+butyellow.onclick=function(){
+    show.setAttribute('class','screenYellow')
+}
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
