ee0aa384b6298c6a9b12760efe51d6a97427d7c9
romanb123 <44100462+romanb123@users.noreply.github.com>
2019-04-09 20:42:48 +0300

    3

--- javascript/to_do_1.js
+++ javascript/to_do_1.js
@@ -1,14 +1,15 @@
 
 
-// function to get the value of missions
+// function to get the value of missions+add
 $(document).ready(function(){
   $(".btn-lg").click(function(){
   var value= $("#usr").val();
   $(".list-group").append(`<li class="list-group-item">${value}
-  <button type="button" class="btn btn-dark">delete</button></li>`) 
-  $(".btn-dark").click(function(){
+  <button type="button" class="btn btn-dark">delete</button></li>`)  
+  });
+  // function to remove mission
+  $(".list-group").on('click','.btn-dark',function(){
   $(this).parent().hide();
     });
-  });
 });
 
